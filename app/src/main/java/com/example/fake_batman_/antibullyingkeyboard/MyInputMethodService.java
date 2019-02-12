479c0d938cf9830aa2641fbcfc6529f4c2eb9359
Harshit Maheshwari <harshitmah98@gmail.com>
2019-02-13 00:30:00 +0530

    Created minimalist keyboard

---
+++ app/src/main/java/com/example/fake_batman_/antibullyingkeyboard/MyInputMethodService.java
@@ -0,0 +1,82 @@
+package com.example.fake_batman_.antibullyingkeyboard;
+
+import android.app.Service;
+import android.content.Intent;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.os.IBinder;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+    public MyInputMethodService() {
+    }
+
+    @Override
+    public View onCreateInputView() {
+        KeyboardView keyboardView = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.alphabets_pad);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+
+    @Override
+    public void onPress(int primaryCode) {
+
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+        if (inputConnection != null){
+            switch (primaryCode){
+                case Keyboard.KEYCODE_DELETE:
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+                    if(TextUtils.isEmpty(selectedText)){
+                        inputConnection.deleteSurroundingText(1, 0);
+                    }else{
+                        inputConnection.commitText("", 1);
+                    }break;
+
+                default:
+                    char code = (char)primaryCode;
+                    inputConnection.commitText(String.valueOf(code), 1);
+
+            }
+        }
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
